66aa0ab2c9829fb59391f71cfbc85b7e373ee7ca
LokeshKvms <loki.kvms@gmail.com>
2025-06-03 14:14:10 +0530

    Played with Tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,10 @@
 <?php
 
+uses(
+    Tests\TestCase::class,
+    Illuminate\Foundation\Testing\RefreshDatabase::class,
+)
+    ->in('Unit');
 /*
 |--------------------------------------------------------------------------
 | Test Case
